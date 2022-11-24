f5348def21be797acfaa0865bfc0e822fa169580
NicoG <guillen.nicole@pucp.edu.pe>
2022-11-24 01:54:32 -0500

    spoofing

---
+++ src/main/java/net/floodlightcontroller/intranetattack/Dao/Imp/SpoofingDaoImp.java
@@ -0,0 +1,15 @@
+package net.floodlightcontroller.intranetattack.Dao.Imp;
+
+import net.floodlightcontroller.intranetattack.Dao.SpoofingDao;
+
+public class SpoofingDaoImp implements SpoofingDao {
+    @Override
+    public String getUserIp(String ip) {
+        return null;
+    }
+
+    @Override
+    public String getResourceIdByIp(String ip) {
+        return null;
+    }
+}
