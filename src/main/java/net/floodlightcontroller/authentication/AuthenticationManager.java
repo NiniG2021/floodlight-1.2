16485a5c4c80ae569e84c337a63cec1b90891e6b
Stephania1807 <stephania.guzman@pucp.edu.pe>
2022-12-01 19:47:24 -0500

    autenticacion

--- src/main/java/net/floodlightcontroller/authentication/AuthenticationManager.java
+++ src/main/java/net/floodlightcontroller/authentication/AuthenticationManager.java
@@ -6,11 +6,17 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.util.AppCookie;
 import net.floodlightcontroller.devicemanager.IDeviceService;
 import net.floodlightcontroller.restserver.IRestApiService;
 import net.floodlightcontroller.routing.IRoutingService;
 import net.floodlightcontroller.staticflowentry.IStaticFlowEntryPusherService;
 import net.floodlightcontroller.topology.ITopologyService;
+import org.projectfloodlight.openflow.protocol.OFFactories;
+import org.projectfloodlight.openflow.protocol.OFFactory;
+import org.projectfloodlight.openflow.protocol.OFVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,15 +25,38 @@ import java.util.Map;
 
 public class AuthenticationManager implements IFloodlightModule{
 
+    private static final short APP_ID = 10;
+
+    static {
+        AppCookie.registerApp(APP_ID, "AuthenticationManager");
+    }
+
+    // Logger for module
+    protected static Logger log = LoggerFactory.getLogger(AuthenticationManager.class);
+
+    // Services to use by module
+    protected IFloodlightProviderService floodlightProviderService;
+    protected IDeviceService deviceService;
+    protected ITopologyService topologyService;
+    protected IOFSwitchService switchService;
+    protected IRoutingService routingService;
+    protected IRestApiService restApiService;
+    protected IStaticFlowEntryPusherService sfp;
+
+    private OFFactory factory = OFFactories.getFactory(OFVersion.OF_13);
+
     // IFloodlightModule
     @Override
     public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
         return null;
     }
 
     @Override
     public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
         Map<Class<? extends IFloodlightService>, IFloodlightService> m = new HashMap<Class<? extends IFloodlightService>, IFloodlightService>();
+        //l.add(IVirtualNetworkService.class);
+        //return l;
         return null;
     }
 
@@ -46,11 +75,16 @@ public class AuthenticationManager implements IFloodlightModule{
 
     @Override
     public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProviderService = context.getServiceImpl(IFloodlightProviderService.class);
+        deviceService = context.getServiceImpl(IDeviceService.class);
+        topologyService = context.getServiceImpl(ITopologyService.class);
+        switchService = context.getServiceImpl(IOFSwitchService.class);
+        routingService = context.getServiceImpl(IRoutingService.class);
+        restApiService = context.getServiceImpl(IRestApiService.class);
+        sfp = context.getServiceImpl(IStaticFlowEntryPusherService.class);
 
     }
 
     @Override
-    public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
-
-    }
+    public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {}
 }
